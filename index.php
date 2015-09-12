f372ff464a40d526784750f050737bcf3676432f
Mattias Hint <hintmattias@gmail.com>
2015-09-12 16:42:14 +0300

    Tere maailm [Delivers #102991504]

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!doctype html>
-<html lang="et">
-<head>
-    <meta charset="UTF-8">
-    <title>Task 1</title>
-</head>
-<body>
-<h1>Tere maailm!</h1>
-</body>
-</html>
+<?php
+
+echo 'Tere maailm!';
+
+?>
