61b1b3e4254f899939d68cbf53075be0007a2de9
Mattias Hint <hintmattias@gmail.com>
2015-09-13 21:22:58 +0300

    Navigeerimatu link [Delivers #103272056]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -12,6 +12,10 @@
 
 <a href="javascript:AlertIt();">Tere maailm</a>
 
+<br>
+
+<a href="http://www.khk.ee" id="myLink" onclick="Function(); alert('J‰‰me siia'); return false;">J‰‰me siia</a>
+
 <script type="text/javascript">
     function myFunction() {
         alert('Tere maailm');
@@ -22,7 +26,6 @@
         if (answer)
             window.location="http://www.khk.ee";
     }
-
 
 
 </script>
