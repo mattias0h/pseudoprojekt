1b4de0d20f3e62fc246f8faa24f23d083dc3bf26
Mattias Hint <hintmattias@gmail.com>
2015-09-13 21:34:35 +0300

    Kass koeraks [Delivers #103272488]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -16,6 +16,10 @@
 
 <a href="http://www.khk.ee" id="myLink" onclick="Function(); alert('J‰‰me siia'); return false;">J‰‰me siia</a>
 
+<br>
+
+<img src="http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg" alt="" onclick="changeSource()">
+
 <script type="text/javascript">
     function myFunction() {
         alert('Tere maailm');
@@ -27,6 +31,10 @@
             window.location="http://www.khk.ee";
     }
 
+    function changeSource() {
+        var image = document.querySelectorAll("img")[0];
+        image.src = image.src.replace("http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg","http://cdn.playbuzz.com/cdn/83c1b6fb-2476-4865-938f-ed8a1caee854/495e5e87-ae72-4380-8184-0a38d901e5a6.jpg");
+    }
 
 </script>
 
