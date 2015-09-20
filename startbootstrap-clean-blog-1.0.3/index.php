a4cfd77b697bf835d8e9c2c90b3cb9d4705fc3e2
Mattias Hint <hintmattias@gmail.com>
2015-09-20 18:01:44 +0300

    28. Fake it until you make it [Delivers #103744892]

--- startbootstrap-clean-blog-1.0.3/index.php
+++ startbootstrap-clean-blog-1.0.3/index.php
@@ -1,3 +1,9 @@
+<?php
+
+$posts = array('title' => 'Man must explore, and this is exploration at its greatest', 'subtitle' => 'Problems look mighty small from 150 miles up', 'author' => 'Start Bootstrap', 'date' => 'September 24, 2014');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -92,13 +98,13 @@
                 <div class="post-preview">
                     <a href="post.html">
                         <h2 class="post-title">
-                            Man must explore, and this is exploration at its greatest
+                            <?php echo $posts['title']?>
                         </h2>
                         <h3 class="post-subtitle">
-                            Problems look mighty small from 150 miles up
+                            <?php echo $posts['subtitle']?>
                         </h3>
                     </a>
-                    <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
+                    <p class="post-meta">Posted by <a href="#"><?php echo $posts['author']?></a> on <?php echo $posts['date']?></p>
                 </div>
                 <hr>
                 <!-- Pager -->
