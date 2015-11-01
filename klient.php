f60698c441c1c8ade3991d3b28bb8e4437766288
mattias0h <hintmattias@gmail.com>
2015-11-01 14:28:57 +0200

    38. Tabeli sorteerimine [Delivers #{number]]

--- klient.php
+++ klient.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <title>getJSON</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="gs_sortable.js"></script>
 </head>
 <body>
 
-<table border="1"></table>
+<table border="1" id="my_table">
+    <thead>
+    <tr>
+        <th>Eesnimi</th>
+        <th>Perenimi</th>
+        <th>Aadress</th>
+    </tr>
+    </thead>
+</table>
 
-<script>
+<script type="text/javascript">
 
     $(document).ready(function() {
         $.getJSON("api.php", function(data) {
@@ -18,6 +27,9 @@
             });
         });
     });
+
+    var TSort_Data = new Array ('my_table', 's', 's', 's');
+    tsRegister();
 
 </script>
 
