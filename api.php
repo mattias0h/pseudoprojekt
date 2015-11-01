ae04f20378760a35c8a8e89a75d953672ed7a78e
mattias0h <hintmattias@gmail.com>
2015-11-01 14:01:58 +0200

    37. Emaili valideerimine serveripoolel ka [Delivers #{number]]

--- api.php
+++ api.php
@@ -1,5 +1,27 @@
 <?php
 
+if (!empty($_GET['command']) && $_GET['command'] == 'emailvalidation') {
+    if (!empty($_GET['email'])) {
+        echo (int)validate_email($_GET['email']);
+    } else {
+        die('email parameeter puudub');
+    }
+    exit();
+}
+
+function validate_email($email) {
+    $exp = "^[a-z\'0-9]+([._-][a-z\'0-9]+)*@([a-z0-9]+([._-][a-z0-9]+))+$";
+    if (eregi($exp,$email)) {
+        if (checkdnsrr(array_pop(explode("@",$email)),"MX")) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
 $arr = array(
     array(
         "eesnimi" => "Juka",
--- meiliaadressi_kontrollimine.php
+++ meiliaadressi_kontrollimine.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Maili kontroll</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
 
@@ -11,15 +12,22 @@
 
 <script type="text/javascript">
     function checkEmail() {
-
         var email = document.getElementById('email');
         var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
         if (!filter.test(email.value)) {
             alert('Teie email ei vasta nõuetele!');
             email.focus;
         } else {
             alert('Teie email on nõuetele vastav!')
+            $.get("api.php?command=emailvalidation&email=" + $('#email').val(), function (value) {
+                if (value == 1) {
+                    alert('Domeen eksisteerib');
+                } else if (value == 0) {
+                    alert('Domeen puudub');
+                } else {
+                    alert('Viga:' + value);
+                }
+            });
         }
     }
 </script>
